30371504ea3febb6fc5b0552889bc926d2ba6753
Josh Myers <josh.myers@photobox.com>
2015-02-11 17:13:17 +0000

    Allow for multiple s3cmd users.

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,23 +13,27 @@ package "python-requests"
 
 package "s3cmd"
 
-if node['s3cmd']['config_dir']
-  home_folder = node['s3cmd']['config_dir']
-else
-  home_folder = node['etc']['passwd'][node['s3cmd']['user']]['dir']
-end
+node['s3cmd']['users'].each do |user|
 
-template "#{home_folder}/.s3cfg" do
-  source "s3cfg.erb"
-  variables(
-    :access_key =>  node['s3cmd']['access_key'],
-    :secret_key =>  node['s3cmd']['secret_key'],
-    :gpg_passphrase =>  node['s3cmd']['gpg_passphrase'],
-    :bucket_location =>  node['s3cmd']['bucket_location'],
-    :https =>  node['s3cmd']['https'],
-    :encrypt =>  node['s3cmd']['encrypt']
-  )
-  owner node['s3cmd']['user']
-  group node['s3cmd']['user']
-  mode 0600
+  home = user == "root" ? "/root" : "/home/#{user}"
+
+  if File.exists? home
+
+    template "#{home}/.s3cfg" do
+      source "s3cfg.erb"
+      variables(
+        :access_key =>  node['s3cmd']['access_key'],
+        :secret_key =>  node['s3cmd']['secret_key'],
+        :gpg_passphrase =>  node['s3cmd']['gpg_passphrase'],
+        :bucket_location =>  node['s3cmd']['bucket_location'],
+        :https =>  node['s3cmd']['https'],
+        :encrypt =>  node['s3cmd']['encrypt']
+      )
+      owner user
+      group user
+      mode 0600
+    end
+  else
+    warn %Q(Home folder "#{home}" doesn't exist, skipping s3cmd for user "#{user}")
+  end
 end
